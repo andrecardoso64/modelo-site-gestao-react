333554aa8e14bc76fc6a08a536571d7d482cf47b
Frusciante <Frusciante@gmail.com>
2026-06-17 21:59:19 -0300

    listing user

--- src/ListarUsuarios.jsx
+++ src/ListarUsuarios.jsx
@@ -4,25 +4,36 @@ import { useState, useEffect } from "react"
 
 function ListarUsuarios(){
 
-    const [usuario, setUsuario] = useState([])
+    const [users, setUsers] = useState([])
 
     //https://jsonplaceholder.typicode.com/users
     //https://spring-boot-treina-recife-turma-11-production.up.railway.app/api/v1/swagger-ui/index.html
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users').then((resp) => resp.json()).then((data) => setUsuario(data));
-        //fetch('https://spring-boot-treina-recife-turma-11-production.up.railway.app/api/v1/public/usuarios').then((resp) => resp.json()).then((data) => setUsuario(data));
-    });
+    const mostrarUsuarios = async () => {
+        try {
+            const usuarios = await fetch('https://spring-boot-treina-recife-turma-11-production.up.railway.app/api/v1/public/usuarios');
+            const dados = await usuarios.json();
+            console.log(dados);
+            setUsers(dados.content);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {mostrarUsuarios()}, [])
     
+    /*fetch('https://spring-boot-treina-recife-turma-11-production.up.railway.app/api/v1/public/usuarios').then((resp) => resp.json()).then((data) => setUsuario(data));
+
+    });*/
 
     return (
         <div>
             <div><h2>Lista de Usuarios</h2></div>
             <ul>
-               {usuario.map((user) => (<li className="my-2">
-                        <div>{user.nome}</div>
-                        <div>Email: {user.email}</div>
-                    </li>))}
+                {
+                    users.map((user) => (<li>{user.id}-{user.nome}</li>))
+                }
             </ul>
         </div>
     )
